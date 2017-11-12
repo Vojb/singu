34cbc1118f8474ecdd35612dc70876056135df05
Robert Barlin <robert.barlin@jayway.com>
2017-11-12 19:30:20 +0100

    motors stubb

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #define STATE_MOVING_STILL
 #define STATE_SEARCHING_PATH
 
+#define DISTANCE_MIN 30 
+
 uint8_t _state = STATE_STARTING
 
 void setup()
@@ -12,13 +14,47 @@ void setup()
 
 }
 
+bool distance_too_close()
+{
+  return distance_current()
+}
+
+float distance_current()
+{
+  return 0;
+}
+
 void handle_state_starting()
 {
 }
 
-void handle_state_moving_()
+
+void motors_left_stop()
 {
 }
+
+void motors_right_stop()
+{
+}
+
+void motors_stop()
+{
+}
+
+void handle_state_moving_still()
+{
+  motors_stop();
+}
+
+void handle_state_moving_forward()
+{
+  if (distance_too_close())
+  {
+    state_set(STATE_MOVING_STILL);
+    return;
+  }
+}
+
 
 void handle_state()
 {
