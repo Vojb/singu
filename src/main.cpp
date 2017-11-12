5429540b8f42689b6c5a8806f7f2b90a527ae347
Robert Barlin <robert.barlin@jayway.com>
2017-11-12 19:11:43 +0100

    basic state handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
-setup()
+#define STATE_STARTING
+#define STATE_MOVING_FORWARD
+#define STATE_MOVING_SPINNING
+#define STATE_MOVING_BACKWARD
+#define STATE_MOVING_STILL
+#define STATE_SEARCHING_PATH
+
+uint8_t _state = STATE_STARTING
+
+void setup()
 {
 
 }
 
-loop()
+void handle_state_starting()
 {
+}
 
+void handle_state_moving_()
+{
+}
+
+void handle_state()
+{
+  switch(_state)
+  {
+
+  }
+}
+
+void loop()
+{
+  handle_state();
 }
